2936a3d199d3fc37538739fc99b0077a064e7cff
Farhan M <farhan9aqaei@gmail.com>
2020-03-24 23:24:48 +0430

    Init commit

---
+++ src/models/user.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error("Email is invalid");
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        trim: true,
+        validate(value) {
+            if(value.toLowerCase().includes("password")) {
+                throw new Error("Password cannot contain 'password'")
+            }
+        }
+    }
+}, {
+    timestamps: true
+});
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    delete userObject.password;
+
+    return userObject;
+}
+
+userSchema.pre("save", async function(next) {
+    const user = this;
+
+    if(user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
