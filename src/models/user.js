c3826e7f7713670688b810a960ae30ee222e3a94
Farhan M <farhan9aqaei@gmail.com>
2020-04-29 14:52:31 +0430

    CRUD operation modified

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -30,18 +31,52 @@ const userSchema = new mongoose.Schema({
                 throw new Error("Password cannot contain 'password'")
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 }, {
     timestamps: true
-});
+}); 
 
 userSchema.methods.toJSON = function () {
     const user = this;
     const userObject = user.toObject();
 
     delete userObject.password;
+    delete userObject.tokens;
+    delete userObject.avatar;
 
     return userObject;
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+
+    return token;
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if(!user) {
+        throw new Error("Invalid e-mail")
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+        throw new Error("Unable to login");
+    }
+
+    return user;
 }
 
 userSchema.pre("save", async function(next) {
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -7,13 +8,40 @@ router.post("/users", async (req, res) => {
 
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
+
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
-router.get("/users", async (req, res) => {
+router.post("/users/login", async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
+    } catch {
+        res.status(400).send();
+    }
+});
+
+router.post("/users/logout", auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+    res.send(req.user);
+});
+
+router.get("/users", auth, async (req, res) => {
     try {
        const users = await User.find();
        res.send(users);
@@ -22,4 +50,33 @@ router.get("/users", async (req, res) => {
     }
 });
 
-module.exports = router;
+router.patch("/users/me", auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["name", "email", "password"];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if(!isValidOperation) {
+        return res.status(400).send({error: "Invalid Updates!"})
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update]);
+        await req.user.save();
+        res.send(req.user);
+
+    } catch(e) {
+        res.status(400).send(e);
+    }
+});
+
+router.delete("/users/me", auth, async (req, res) => {
+    try {
+        await req.user.remove();
+        res.send(req.user);
+
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+module.exports = router; 
